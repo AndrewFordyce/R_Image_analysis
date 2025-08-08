54b9339e67ba5c4f275409771a11977c3bbd5fcd
AndrewFordyce <113497336+AndrewFordyce@users.noreply.github.com>
2025-08-08 21:42:40 +1200

    Update 220914_R_Image_Analysis_Generic.R

--- 220914_R_Image_Analysis_Generic.R
+++ 220914_R_Image_Analysis_Generic.R
@@ -1,7 +1,5 @@
 #set wd, change as needed
 setwd("Working directory")
-
-#ensure libraries are loaded
 library(dplyr)
 library(imager)
 library(bmp)
@@ -9,14 +7,11 @@ library(OpenImageR)
 library(tidyverse)
 library(readr)
 library(tibble)
-
 #note, change 'PATH' variable to folder where images are from first
 #seperate directory into sections e.g. C:/Users/User/Desktop would become "C:", "Users", "User" "Desktop", fsep ="/"
 PATH <- file.path("C:", "Users", "...", 
                   fsep = "/")
-
 FOLDERS <- list.dirs(PATH, full.names = T, recursive = F) #list folders in PATH
-
 #loads all .BMP files within 'Folders'
 files<-list.files(FOLDERS, pattern = ".BMP", full.names = T, ) %>%
   lapply(function(file) {
@@ -29,7 +24,6 @@ files<-list.files(FOLDERS, pattern = ".BMP", full.names = T, ) %>%
     names(Crop_data)[3] <-paste("r")
     names(Crop_data)[4] <-paste("g")
     names(Crop_data)[5] <-paste("b")
-    
     #converting rgb to luminance according to CIE XYZ colour
     Crop_data$Lum <- 0.2126*(Crop_data$r)^2.2 + 0.7152 * (Crop_data$g)^2.2 + 0.0722* (Crop_data$b)^2.2
     #stats of luminance
@@ -37,7 +31,6 @@ files<-list.files(FOLDERS, pattern = ".BMP", full.names = T, ) %>%
     SD_Lum <- sd(Crop_data$Lum)
     Data <- data.frame(Mean_Lum, SD_Lum)
   })
-
 #output file information as data table
 Files_output <-  files[[1]]
 lapply(2:length(files), function(i) {Files_output <<- rbind(Files_output, files[[i]])}) 
@@ -45,5 +38,5 @@ Files_output$Name <- list.files(FOLDERS, pattern = ".BMP") # add file names
 Files_output <- Files_output %>% relocate (Name, .before = Mean_Lum) #move file names to front
 
 #this will save table as Output.CSV to the folder specified by 'PATH'
-#NOTE: COLUMN ORDER WILL BE DETERMINED BY FILE NAME alphabetically e.g. Test 1, Test 10, Test 2, Test 3
 write.table(Files_output, paste(PATH, "Output.csv", sep = "/"), row.names = F, col.names = T, sep = ',')
+
